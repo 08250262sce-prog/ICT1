3f933d4776ebe980d77d8a92143d53e193f65751
User <08250262.sec.@rub.edu.bt>
2026-03-20 16:19:51 +0600

    laerning new activity

---
+++ Demo/activity.py
@@ -0,0 +1,13 @@
+name=input("Entre your name:")
+print("Goodmorning"+name)
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second name:"))
+addition_result=num1+num2
+substraction_result=num1-num2
+multiplication_result=num1*num2
+division_result=num1/num2
+
+print("Addition:",addition_result)
+print("Substraaction:",substraction_result)
+print("Multiplication:",multiplication_result)
+print("Division:",division_result)
