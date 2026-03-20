ea242c2fdc8386f391317a485194e4444c4be298
User <08250262.sec.@rub.edu.bt>
2026-03-20 15:38:00 +0600

    learning area os the rectangle

---
+++ Demo/area of rectangle.py
@@ -0,0 +1,7 @@
+L=float(input("Enter the length of recatngle:"))
+B=float(input("Enter the breath of reactangle:"))
+area=L*B
+print("The area of  the rectangle is:",area)
+print("The area of the rectangle is:"+str(area))
+      
+
