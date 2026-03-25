438b68b7446eb6354e164f9fa1668ac7cb5a7e0f
User <08250262.sec.@rub.edu.bt>
2026-03-25 10:10:43 +0600

    learning operational example

--- Demo/conditionalEx.py
+++ Demo/conditionalEx.py
@@ -2,11 +2,12 @@ Marks1=float(input("Enter the marks of first subject:"))
 Marks2=float(input("Enter the marks of second subject:"))
 Marks3=float(input("Enyer the marks of thhird subject:"))
 average=(Marks1+Marks2+Marks3)/3
+print("The average marks is:%.2f" %average)
 
 if(average >= 90 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
     print("Grade:A")
 elif(average >= 80 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
-    print("Grade:B")
+    print("Grade:B") 
 elif(average >= 70 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
     print("Grade:C")
 elif(average >= 60 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
