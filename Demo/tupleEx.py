c845ac5f875e083bd421f9c02cdf832828287d31
User <08250262.sec.@rub.edu.bt>
2026-04-03 15:09:06 +0600

    learning tuple

--- Demo/tupleEx.py
+++ Demo/tupleEx.py
@@ -11,8 +11,11 @@ concatented_tuple=my_tuple+new_tup #Concatenating two tuples
 print(concatented_tuple)#Concatenated tuple of my_tuple and new_tup
 print(concatented_tuple[2:6:2])#Slicing the tuple from index 2 to 5 with a step of 2
 print(concatented_tuple[::-1])#Reversing the tuple
-del my_tuple#Deleting the tuple
 print(my_tuple) #This will raise an error as my_tuple has been deleted
 
 
+n=concatented_tuple[2:7:4]#Slicing the tuple from index 2 to 6 with a step of 4
+print(n[::-1])#Reversing the sliced tuple
 
+print(concatented_tuple[6:1:-4])#Slicing the tuple from index 6 to the end
+print(concatented_tuple[::-4])#Slicing the tuple from index 1 to 5 with a step of 4
